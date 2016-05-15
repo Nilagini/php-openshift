2b8a6cf3ef3e26b71a6f26309dff36616165c375
T.Nilasini <Nilagini@users.noreply.github.com>
2016-05-15 12:30:14 +0530

    Update Config1.php

--- Config1.php
+++ Config1.php
@@ -4,8 +4,13 @@
   // define('DB_PASSWORD', '');
   // define('DB_HOST', 'localhost');
   // define('DB_NAME', 'bbms');
-	define('DB_USERNAME', 'adminRXeACtm');
-    define('DB_PASSWORD', 'GEKWxRaUn3gM');
-    define('DB_HOST', 'https://php-bloodbankms.rhcloud.com');
-    define('DB_NAME', 'php');
+//	define('DB_USERNAME', 'adminRXeACtm');
+ //   define('DB_PASSWORD', 'GEKWxRaUn3gM');
+   // define('DB_HOST', 'https://php-bloodbankms.rhcloud.com');
+   // define('DB_NAME', 'php');
+    define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+   define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+   define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+   define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+   define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 ?>
