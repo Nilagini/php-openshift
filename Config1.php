4a8905311d4d2fe91512514c1bb7aa4bf9d258f6
T.Nilasini <Nilagini@users.noreply.github.com>
2016-05-15 12:18:15 +0530

    Update Config1.php

--- Config1.php
+++ Config1.php
@@ -6,6 +6,6 @@
   // define('DB_NAME', 'bbms');
 	define('DB_USERNAME', 'adminRXeACtm');
     define('DB_PASSWORD', 'GEKWxRaUn3gM');
-    define('DB_HOST', 'https://php-bloodbankms.rhcloud.com');
+    define('DB_HOST', 'localhost');
     define('DB_NAME', 'php');
 ?>
