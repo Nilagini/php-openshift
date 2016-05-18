75092295ae57d28a3b8220fc5ba750ecbb52a82c
T.Nilasini <Nilagini@users.noreply.github.com>
2016-05-18 22:36:53 +0530

    Update Config1.php

--- Config1.php
+++ Config1.php
@@ -4,8 +4,7 @@
   // define('DB_PASSWORD', '');
   // define('DB_HOST', 'localhost');
   // define('DB_NAME', 'bbms');
-  echo "nilasinithiru";
-	define('DB_USERNAME', 'adminRXeACtm');
+define('DB_USERNAME', 'adminRXeACtm');
  define('DB_PASSWORD', 'GEKWxRaUn3gM');
 define('DB_HOST', '127.6.108.2');
  define('DB_NAME', 'php');
